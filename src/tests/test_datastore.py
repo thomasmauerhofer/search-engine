1e195f111e06b73428ace1bc93c41d78cb59b7a0
Thomas Mauerhofer <thomas.mauerhofer@student.tugraz.at>
2018-04-25 11:26:45 +0200

    Move test

--- src/tests/test_datastore.py
+++ src/tests/test_datastore.py
@@ -1,6 +1,8 @@
 from unittest import TestCase
 
 import engine.datastore.datastore_utils.crypto as crypto
+from engine.api import API
+from engine.datastore.structure.section import IMRaDType
 
 
 class TestCrypto(TestCase):
@@ -11,3 +13,15 @@ class TestCrypto(TestCase):
         self.assertTrue(crypto.verify(encrypt, password))
         self.assertFalse(crypto.verify(encrypt, "wrong_password"))
 
+    def test_simple_ranking(self):
+        queries = {IMRaDType.INDRODUCTION.name: "paper",
+                   IMRaDType.BACKGROUND: "",
+                   IMRaDType.METHODS.name: "inhom scenario allow user control home",
+                   IMRaDType.RESULTS.name: "",
+                   IMRaDType.DISCUSSION.name: ""}
+
+        settings = {"importance_sections": True}
+
+        api = API()
+        ret = api.get_papers_simple_ranking(queries, settings)
+        self.assertGreater(len(ret), 0)
